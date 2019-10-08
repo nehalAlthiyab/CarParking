5ec9545b37008e6c7bb707d5fa6da1b22542042e
Nehal Althiyab <nehal1997.n1@gmail.com>
2019-10-08 10:18:29 +0300

    add all the files

---
+++ EmployeeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        private readonly EmployeeContext _context;
+
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+
+        // GET: api/<Employees>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return employees;
+        }
+
+        // GET api/<Employees>id 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employees= await _context.Employees.FindAsync(id);
+
+            if (employees== null)
+            {
+                return NotFound();
+            }
+
+            return employees;
+        }
+
+        // POST api/<Employees>
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return NotFound();
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.ID }, employee);
+        }
+
+        // PUT api/<Employees>/id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<Employees>/id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var todoItem = await _context.Employees.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ EmployeeAPI/Models/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeAPI.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new EmployeeContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<EmployeeContext>>()))
+            {
+                // Look for any movies.
+                if (context.Employees.Any())
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
